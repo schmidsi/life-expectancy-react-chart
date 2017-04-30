8f4cf01ec20bd15343cffe6b72bd0679fcb2caf1
Simon Emanuel Schmid <simon@oscillate.ch>
2017-04-30 21:03:10 +0200

    filter data, format chart

--- src/index.js
+++ src/index.js
@@ -3,7 +3,21 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+import { csv as d3csv } from 'd3-request';
+
+d3csv('./who-life-expectancy-by-country.csv', (data) => {
+  const wrangledData = data
+    .filter(d => d.Year === '2015')
+    .map(d => ({
+      country: d.Country,
+      expectancy: parseFloat(d['Life expectancy at birth']),
+    }))
+    .sort((a, b) => (a.expectancy >= b.expectancy) ? -1 : 1)
+    ;
+
+  ReactDOM.render(
+    <App data={wrangledData} />,
+    document.getElementById('root'),
+  );
+});
+
